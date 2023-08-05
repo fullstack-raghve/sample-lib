c693525cd5776136f0eb1deb9c80813551db70df
Raghvendra Singh <raghvendra.singh@techtreeit.com>
2023-08-05 14:19:31 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreLibModule } from 'core-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,CoreLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
